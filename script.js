17283405bcea6538710850e3d1e5233d052bafc2
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2018-06-22 18:41:39 -0500

    Primera Version!

---
+++ script.js
@@ -0,0 +1,23 @@
+alert ("buenas, para lo de que funcione");
+var dia1="Lunes";
+var dia2="Martes";
+var dia3="Miércoles";
+var ineficiente="ineficiente";
+var dia7= "domingo";
+  
+var dias=["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+
+var nombre_array = ["valor1","valor2", "valor", "valorN"];
+
+var diaSeleccionado=dias[0]; 
+var otroDia =dias[5];
+
+var clienteRegistrado=false;
+var ivaIncluido=true;
+
+var numero1=3;
+var numero2=4;
+
+var numero=5;
+++numero;
+alert(numero);
